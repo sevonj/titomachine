afa31ad2af6d82c3c1ec12d71906a4a60ac4e973
sevonj <100710152+sevonj@users.noreply.github.com>
2023-04-14 18:52:50 +0300

    fixed disassembler

--- src/gui/gui_emulator/disassembler.rs
+++ src/gui/gui_emulator/disassembler.rs
@@ -44,24 +44,43 @@ pub enum Opcode {
     HCF = 0x72,
 }
 
-fn addr2string(ri: i32, addr: i32) -> String {
+fn second2string(m: i32, ri: i32, addr: i32) -> String {
     let mut str = String::new();
+    let mut m = m;
+    if addr == 0 && ri != 0 {
+        m += 1;
+    }
+    match m {
+        0 => str += "=",
+        1 => str += " ",
+        2 => str += "@",
+        3 => {
+            // @(R1) results to this
+            return format!("@({})", reg2string(ri));
+        }
+        _ => str += "wtf‽",
+    }
     if ri == 0 {
-        return addr.to_string();
+        str += &addr.to_string();
+        return str;
     }
     if addr != 0 {
         str += &addr.to_string();
         str += "(";
     }
-    match ri {
-        6 => str += "SP",
-        7 => str += "FP",
-        _ => str += format!("R{}", ri).as_str(),
-    }
+    str += reg2string(ri).as_str();
     if addr != 0 {
         str += ")";
     }
     str
+}
+
+fn reg2string(r: i32) -> String {
+    match r {
+        6 => "SP, ".into(),
+        7 => "FP, ".into(),
+        _ => format!("R{}", r),
+    }
 }
 
 fn rj2string(r: i32) -> String {
@@ -72,123 +91,87 @@ fn rj2string(r: i32) -> String {
     }
 }
 
-fn mode2string(m: i32) -> String {
-    match m {
-        0 => "=".into(),
-        1 => " ".into(),
-        2 => "@".into(),
-        _ => "wtf‽".into(),
-    }
-}
-
-fn ri2string(r: i32) -> String {
-    match r {
-        0 => "".into(),
-        6 => "SP".into(),
-        7 => "FP".into(),
-        _ => format!("R{}", r),
-    }
-}
-
 pub fn instruction_to_string(input_instr: i32) -> String {
     let mut retstr = String::new();
 
     let opcode = input_instr >> 24;
     let rj = (input_instr >> 21) & 0x7;
-    let mode; // = (input_instr >> 19) & 0x3;
+    let mode = (input_instr >> 19) & 0x3;
     let ri = (input_instr >> 16) & 0x7;
     let addr = (input_instr & 0xffff) as i16 as i32; // these casts catch the sign
-
-    match addr == 0 {
-        true => mode = ((input_instr >> 19) & 0x3),
-        false => mode = ((input_instr >> 19) & 0x3) - 1,
-    }
 
     match FromPrimitive::from_i32(opcode) {
         Some(Opcode::NOP) => retstr += "NOP",
         Some(Opcode::STORE) => {
             retstr += "STORE ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode + 1).as_str(); // Notice +1
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::LOAD) => {
             retstr += "LOAD  ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::IN) => {
             retstr += "IN    ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            println!("IN mode: {}", mode);
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::OUT) => {
             retstr += "OUT   ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::ADD) => {
             retstr += "ADD   ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::SUB) => {
             retstr += "SUB   ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::MUL) => {
             retstr += "MUL   ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::DIV) => {
             retstr += "DIV   ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::MOD) => {
             retstr += "MOD   ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::AND) => {
             retstr += "AND   ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::OR) => {
             retstr += "OR    ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::XOR) => {
             retstr += "XOR   ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::SHL) => {
             retstr += "SHL   ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::SHR) => {
             retstr += "SHR   ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::NOT) => {
             retstr += "NOT   ";
@@ -197,96 +180,90 @@ pub fn instruction_to_string(input_instr: i32) -> String {
         Some(Opcode::SHRA) => {
             retstr += "SHRA  ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::COMP) => {
             retstr += "COMP  ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::JUMP) => {
             retstr += "JUMP  ";
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::JNEG) => {
             retstr += "JNEG  ";
             retstr += rj2string(rj).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::JZER) => {
             retstr += "JZER  ";
             retstr += rj2string(rj).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::JPOS) => {
             retstr += "JPOS ";
             retstr += rj2string(rj).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::JNNEG) => {
             retstr += "JNNEG ";
             retstr += rj2string(rj).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::JNZER) => {
             retstr += "JNZER ";
             retstr += rj2string(rj).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::JNPOS) => {
             retstr += "JNPOS ";
             retstr += rj2string(rj).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::JLES) => {
             retstr += "JLES  ";
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::JEQU) => {
             retstr += "JEQU  ";
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::JGRE) => {
             retstr += "JGRE  ";
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::JNLES) => {
             retstr += "JNLES ";
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::JNEQU) => {
             retstr += "JNEQU ";
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::JNGRE) => {
             retstr += "JNGRE ";
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::CALL) => {
             retstr += "CALL  ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode + 1).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode + 1, ri, addr).as_str();
         }
         Some(Opcode::EXIT) => {
             retstr += "EXIT  ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::PUSH) => {
             retstr += "PUSH  ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::POP) => {
             retstr += "POP   ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode + 1).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::PUSHR) => {
             retstr += "PUSHR ";
@@ -299,14 +276,12 @@ pub fn instruction_to_string(input_instr: i32) -> String {
         Some(Opcode::IEXIT) => {
             retstr += "IEXIT ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::SVC) => {
             retstr += "SVC   ";
             retstr += rj2string(rj).as_str();
-            retstr += mode2string(mode).as_str();
-            retstr += addr2string(ri, addr).as_str();
+            retstr += second2string(mode, ri, addr).as_str();
         }
         Some(Opcode::HLT) => retstr += "HLT   ",
         Some(Opcode::HCF) => retstr += "HCF   ",
