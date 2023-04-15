20dfd7bafc942b2ab255f5f4ec51ba54e7d8503b
sevonj <100710152+sevonj@users.noreply.github.com>
2023-04-15 22:02:35 +0300

    ivt test

--- src/emulator/tests.rs
+++ src/emulator/tests.rs
@@ -69,6 +69,18 @@ fn test_cpu_stack() {
     assert_eq!(regs[5], expected_r5);
 }
 
+/// Verify that IVT entries are loaded correctly
+#[test]
+fn test_loader_ivt() {
+    let prog = compile(include_str!("../../programs/tests/test_loader_ivt.k91").into());
+    let mut cpu = CPU::new();
+    let mut bus = Bus::new();
+    loader::load_program(&mut bus, &mut cpu, &prog);
+    for i in 0..=15 {
+        assert_eq!(cpu.debug_get_ivt(i), 0x1000 + i as i32)
+    }
+}
+
 fn compile(source: String) -> String {
     let mut compiler = Compiler::default();
     compiler.compile(source).unwrap()
